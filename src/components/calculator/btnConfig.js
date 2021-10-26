57359995e0a0d2e304afa4f6fd2a1d9e96016178
Beatriz <beammz@gmail.com>
2021-10-25 23:04:43 -0300

    adding btn config

---
+++ src/components/calculator/btnConfig.js
@@ -0,0 +1,109 @@
+export const BTN_ACTIONS = {
+    ADD: 'ADD',
+    THEME: 'THEME',
+    CALC: 'CALC',
+    DELETE: 'DELETE'
+}
+
+export const btns = [
+    {
+        display: 'C',
+        action: BTN_ACTIONS.DELETE,
+        class: 'btn__op'
+    },
+    {
+        display: '(',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: ')',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '/',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '7',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '8',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '9',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: 'x',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '4',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '5',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '6',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '-',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '1',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '2',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '3',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '+',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '☀️',
+        action: BTN_ACTIONS.THEME,
+        class: 'btn__op'
+    },
+    {
+        display: '0',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '.',
+        action: BTN_ACTIONS.ADD,
+        class: 'btn__op'
+    },
+    {
+        display: '=',
+        action: BTN_ACTIONS.CALC,
+        class: 'btn__op'
+    }
+]
