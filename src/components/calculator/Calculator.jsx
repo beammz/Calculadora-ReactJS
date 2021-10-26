e1aa6f08122064187f68288eaf4bb481a78f9264
Beatriz <beammz@gmail.com>
2021-10-25 23:30:27 -0300

    adding css

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useRef, useEffect, useState} from "react";
 
 import './calculator.css';
 
+import { btns, BTN_ACTIONS} from './btnConfig';
+
 const Calculator = () => {
+
+    const btnsRef = useRef(null);
+    const expRef = useRef(null);
+
+    const [expression, setExpression] = useState('');
+
     return (
-        <div>
-            Calculator
+        <div className="calculator">
+            <div className="calculator__result">
+                <div className="calculator__result__exp"></div>
+                <div className="calculator__result__exp"></div>
+            </div>
+            <div className="calculator__btns">
+                {
+                    btns.map((item, index) => (
+                        <button
+                            key={index}
+                            className={item.class}
+                            
+                        >
+                            {item.display}
+                        </button>
+                    ) )
+                }
+            </div>
         </div>
     )
 }
 
-export default Calculator
+export default Calculator;
